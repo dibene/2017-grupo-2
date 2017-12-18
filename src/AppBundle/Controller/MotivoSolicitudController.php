1c770c7d3d766bf0bfd90f812526c182d91623eb
CAPPELLI, TOMAS <tomascappelli89@gmail.com>
2017-12-18 19:51:15 -0300

    CRUD Paciente, MotivoSolicitud y GrupoDiagnostico

---
+++ src/AppBundle/Controller/MotivoSolicitudController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\MotivoSolicitud;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Motivosolicitud controller.
+ *
+ * @Route("motivosolicitud")
+ */
+class MotivoSolicitudController extends Controller
+{
+    /**
+     * Lists all motivoSolicitud entities.
+     *
+     * @Route("/", name="motivosolicitud_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $motivoSolicituds = $em->getRepository('AppBundle:MotivoSolicitud')->findAll();
+
+        return $this->render('motivosolicitud/index.html.twig', array(
+            'motivoSolicituds' => $motivoSolicituds,
+        ));
+    }
+
+    /**
+     * Creates a new motivoSolicitud entity.
+     *
+     * @Route("/new", name="motivosolicitud_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $motivoSolicitud = new Motivosolicitud();
+        $form = $this->createForm('AppBundle\Form\MotivoSolicitudType', $motivoSolicitud);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($motivoSolicitud);
+            $em->flush();
+
+            return $this->redirectToRoute('motivosolicitud_show', array('id' => $motivoSolicitud->getId()));
+        }
+
+        return $this->render('motivosolicitud/new.html.twig', array(
+            'motivoSolicitud' => $motivoSolicitud,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a motivoSolicitud entity.
+     *
+     * @Route("/{id}", name="motivosolicitud_show")
+     * @Method("GET")
+     */
+    public function showAction(MotivoSolicitud $motivoSolicitud)
+    {
+        $deleteForm = $this->createDeleteForm($motivoSolicitud);
+
+        return $this->render('motivosolicitud/show.html.twig', array(
+            'motivoSolicitud' => $motivoSolicitud,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing motivoSolicitud entity.
+     *
+     * @Route("/{id}/edit", name="motivosolicitud_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, MotivoSolicitud $motivoSolicitud)
+    {
+        $deleteForm = $this->createDeleteForm($motivoSolicitud);
+        $editForm = $this->createForm('AppBundle\Form\MotivoSolicitudType', $motivoSolicitud);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('motivosolicitud_edit', array('id' => $motivoSolicitud->getId()));
+        }
+
+        return $this->render('motivosolicitud/edit.html.twig', array(
+            'motivoSolicitud' => $motivoSolicitud,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a motivoSolicitud entity.
+     *
+     * @Route("/{id}", name="motivosolicitud_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, MotivoSolicitud $motivoSolicitud)
+    {
+        $form = $this->createDeleteForm($motivoSolicitud);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($motivoSolicitud);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('motivosolicitud_index');
+    }
+
+    /**
+     * Creates a form to delete a motivoSolicitud entity.
+     *
+     * @param MotivoSolicitud $motivoSolicitud The motivoSolicitud entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(MotivoSolicitud $motivoSolicitud)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('motivosolicitud_delete', array('id' => $motivoSolicitud->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/GrupoDiagnostico.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * GrupoDiagnostico
+ *
+ * @ORM\Table(name="grupo_diagnostico")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GrupoDiagnosticoRepository")
+ */
+class GrupoDiagnostico
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="string", length=255)
+     */
+    private $descripcion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     * @return GrupoDiagnostico
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string 
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return GrupoDiagnostico
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
