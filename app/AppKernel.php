c317ad407b0de88a099f7660982ab90c2600565b
santiagodibene <dibenesantiago@gmail.com>
2017-12-22 19:54:06 -0300

    Auto stash before rebase of "origin/master"

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
