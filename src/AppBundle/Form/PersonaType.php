4ca7fd56a6b021b038d5fd0ea4ff6130e4a9c99f
CAPPELLI, TOMAS <tomascappelli89@gmail.com>
2018-01-02 11:07:08 -0300

    Choice de Genero en Usuario y Paciente

--- src/AppBundle/Form/PersonaType.php
+++ src/AppBundle/Form/PersonaType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class PersonaType extends AbstractType
 {
@@ -13,7 +14,7 @@ class PersonaType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nombre')->add('apellido')->add('dni')->add('direccion')->add('sexo')->add('nacionalidad')->add('localidad')->add('fechaNacimiento');
+        $builder->add('nombre')->add('apellido')->add('dni')->add('direccion')->->add('sexo', ChoiceType::class, array('choices'  => array('Masculino' => 'Masculino', 'Femenino' => 'Femenino', 'Otros' => 'Otros')))->add('nacionalidad')->add('localidad')->add('fechaNacimiento');
     }
     
     /**
